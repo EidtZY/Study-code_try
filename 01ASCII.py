cfc4dc2beaf08fdf515d34d020c6308b25bed544
EidtZY <52123321+EidtZY@users.noreply.github.com>
2019-07-24 20:04:23 +0800

    '''''''''][

---
+++ 01ASCII.py
@@ -0,0 +1,5 @@
+print('A')
+print(chr(65))
+
+for k in range (33,137):
+    print('ASC CODE (%d) is %c' % (k,chr(k)))
---
+++ test_bin_hex.py
@@ -0,0 +1,11 @@
+num = 142857
+
+print('--二进制:{:b}'.format(num))
+print('--八进制: %o' % num)
+print('--十进制: %d' % num)
+print('--十六进制: %x' % num)
+
+print('--二进制：', bin(num))
+print('--八进制：', oct(num))
+print('--十进制：', num)
+print('--十六进制：', hex(num))
